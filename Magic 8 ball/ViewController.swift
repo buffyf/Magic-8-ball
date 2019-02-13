dae5fe2292a8a023a2e6157b128624be05b07d33
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-02-12 19:52:02 -0600

    Initial Commit

---
+++ Magic 8 ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 ball
+//
+//  Created by Barbara Feinstein on 2/12/19.
+//  Copyright © 2019 Barbara Feinstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

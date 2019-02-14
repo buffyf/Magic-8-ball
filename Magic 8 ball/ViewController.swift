8042bf9a4f7baf788f420c2fec80942831d6d71f
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-02-13 21:06:48 -0600

    Magic 8 Ball App with shake gesture

--- Magic 8 ball/ViewController.swift
+++ Magic 8 ball/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
 
+    var randomBallNumber: Int = 0
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        newBallImage()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        
+                randomBallNumber = Int.random(in: 0 ... 4)
+        
+                imageView.image = UIImage(named: ballArray[randomBallNumber])
+        
+            }
+    
+            override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+                newBallImage()
+            }
+    
 }
-
